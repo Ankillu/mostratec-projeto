070a265bddef018d81f12a7470bfdeca80ed27bd
Angelo Facchin <angelo.jfacchin@hotmail.com>
2023-05-10 15:38:12 -0300

    alterando codigo feito no tinkercad

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,38 +1,39 @@
 #include <LiquidCrystal.h>
 
-// Pino de conexão do sensor de temperatura ao Arduino
+// Inicialização do display LCD
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// Pino do sensor de temperatura
 const int pinSensorTemperatura = A0;
 
-// Pinos de conexão do display LCD ao Arduino
-const int pinRS = 12;
-const int pinE = 11;
-const int pinD4 = 5;
-const int pinD5 = 4;
-const int pinD6 = 3;
-const int pinD7 = 2;
-
-// Inicializa o objeto do display LCD
-LiquidCrystal lcd(pinRS, pinE, pinD4, pinD5, pinD6, pinD7);
-
 void setup() {
-  // Inicializa o display LCD com 16 colunas e 2 linhas
+  // Inicialização do display LCD
   lcd.begin(16, 2);
   lcd.print("Temperatura:");
+
+  // Inicialização da comunicação serial
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Lê o valor analógico do sensor de temperatura
+  // Leitura da tensão do sensor de temperatura
   int valorSensor = analogRead(pinSensorTemperatura);
 
-  // Converte o valor analógico para temperatura em graus Celsius
-  float temperatura = (valorSensor * 5.0 / 1023.0) * 100.0;
+  // Cálculo da temperatura em Celsius
+  float tensao = valorSensor * (5.0 / 1023.0);
+  float temperatura = (tensao - 0.5) * 100;
 
-  // Exibe a temperatura no display LCD
+  // Exibição da temperatura no display LCD
   lcd.setCursor(0, 1);
-  lcd.print("   ");
+  lcd.print("     ");
   lcd.setCursor(0, 1);
   lcd.print(temperatura);
   lcd.print(" C");
 
-  delay(1000);  // Aguarda 1 segundo antes de atualizar a temperatura novamente
+  // Exibição da temperatura no monitor serial
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println(" C");
+
+  delay(1000); // Aguarda 1 segundo antes de ler a temperatura novamente
 }
